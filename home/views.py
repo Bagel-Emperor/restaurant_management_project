ce0ce0ccd2d41e8b60d311a9167375df2217c297
Braden Stephens <bagelemperor@protonmail.com>
2025-08-12 20:34:50 -0600

    Display restaurant name on homepage from settings.py

--- home/views.py
+++ home/views.py
@@ -1,13 +1,24 @@
 
+
 # Import the render function to display templates
 from django.shortcuts import render
+# Import settings to access RESTAURANT_NAME
+from django.conf import settings
 
 # This view renders the homepage using our new styled template
+
 def home_view(request):
-	# 'request' is the HTTP request object
-	# 'home/index.html' is the path to our template
-	# The third argument (context) is optional and can be used to pass data to the template
-	return render(request, 'home/index.html')
+	"""
+	View to render the homepage with the restaurant's name from settings.
+	Args:
+		request: The HTTP request object.
+	Returns:
+		HttpResponse: Rendered homepage with restaurant name in context.
+	"""
+	context = {
+		'restaurant_name': getattr(settings, 'RESTAURANT_NAME', 'Our Restaurant')
+	}
+	return render(request, 'home/index.html', context)
 
 
 # Custom 404 error handler view
--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -19,8 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-s^x_u!itekxd=@a3o3zv4d%hw$j6d#2v358wgltxl8rc(-^t&p'
+
+# Restaurant name for display on homepage
+RESTAURANT_NAME = "Perpex Bistro"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!

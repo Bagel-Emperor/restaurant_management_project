89e63f98a85fcc183360d4b855f50a25945421c0
Braden Stephens <bagelemperor@protonmail.com>
2025-08-14 22:28:53 -0600

    Add Contact Us page

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-# Import the home_view we just created
-from .views import home_view
-from .views import home_view, about_view
-# Import the home_view we just created
-from .views import home_view, about_view
+from .views import home_view, about_view, contact_view
+
 # URL patterns define which view is called for each URL
 urlpatterns = [
-	# '' means the root of this app; home_view will handle it
     # '' means the root of this app; home_view will handle it
     path('', home_view, name='home'),
     # 'about/' for the about page
     path('about/', about_view, name='about'),
+    # 'contact/' for the contact page
+    path('contact/', contact_view, name='contact'),
 ]

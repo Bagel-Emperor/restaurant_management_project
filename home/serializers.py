4ab1175756eeb551f0f29687af9803172fcc4de6
Braden Stephens <bagelemperor@protonmail.com>
2025-08-14 23:41:36 -0600

    Add RestaurantSerializer using ModelSerializer for API registration and validation

---
+++ home/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Restaurant
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = [
+            'id',
+            'name',
+            'owner_name',
+            'email',
+            'phone_number',
+            'address',
+            'city',
+            'created_at',
+        ]

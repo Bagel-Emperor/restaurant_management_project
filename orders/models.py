4d857ad7d7734074958122d32c70541ea8c56857
Braden Stephens <bagelemperor@protonmail.com>
2025-08-26 09:36:38 -0600

    Remove email field from UserProfile model to avoid duplication with User.email

--- orders/models.py
+++ orders/models.py
@@ -9,7 +9,6 @@ from home.models import MenuItem
 class UserProfile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
 	name = models.CharField(max_length=100, blank=True)
-	email = models.EmailField(blank=True)
 	phone = models.CharField(max_length=20, blank=True)
 
 	def __str__(self):

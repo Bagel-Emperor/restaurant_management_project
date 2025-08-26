b370e7dadbc39131bfba47b71014857fe11b8676
Braden Stephens <bagelemperor@protonmail.com>
2025-08-26 09:32:11 -0600

    Add UserProfile model extending Django User with name, email, and phone fields

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,19 @@
 
 from django.db import models
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
 from home.models import MenuItem
+
+# UserProfile model extending Django User
+class UserProfile(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+	name = models.CharField(max_length=100, blank=True)
+	email = models.EmailField(blank=True)
+	phone = models.CharField(max_length=20, blank=True)
+
+	def __str__(self):
+		return self.name or self.user.username
 
 class Customer(models.Model):
 	name = models.CharField(max_length=100, blank=True, null=True)

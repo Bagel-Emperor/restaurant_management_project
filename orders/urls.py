0a015e378047c64275e8a4e232502f86a6a06680
Braden Stephens <bagelemperor@protonmail.com>
2025-08-26 07:52:37 -0600

    Make restaurant name dynamic in templates using settings value

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CustomerOrderListView, OrderCreateView, CustomerListCreateAPIView
+from .views import CustomerOrderListView, CreateOrderView, CustomerListCreateAPIView
 
 urlpatterns = [
 	path('orders/', CustomerOrderListView.as_view(), name='customer-order-list'),
-	path('orders/create/', OrderCreateView.as_view(), name='order-create'),
+	path('orders/create/', CreateOrderView.as_view(), name='order-create'),
 	path('customers/', CustomerListCreateAPIView.as_view(), name='customer-list-create'),
 ]
